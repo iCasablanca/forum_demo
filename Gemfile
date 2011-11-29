f09a8e8ddb9f50cdf950fbdad03c42ec8603d3f0
tamsuiboy <tamsuiboy@hotmail.com>
2011-11-29 14:16:53 +0800

    chapter 8 done

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.0.10'
 gem 'sqlite3'
 gem 'powder'
 gem 'devise'
+gem 'will_paginate'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
-  belongs_to :board
+  belongs_to :board, :counter_cache => true
   belongs_to :user
+  #default_scope :order => 'created_at DESC'
+  scope :recent, :order => 'updated_at DESC' 
 end
